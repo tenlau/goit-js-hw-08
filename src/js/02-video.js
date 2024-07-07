a7a1cb01ce34d01ce84b0ad86b15b53b6ba89c02
LaurentiuSorinTENE <sorin.tene@gmail.com>
2024-07-07 09:15:17 +0300

    Tema 8 - 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle((data) => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime !== null) {
+  player.setCurrentTime(parseFloat(savedTime)).catch(function(error) {
+    console.error('Error setting the current time:', error);
+  });
+}
 
